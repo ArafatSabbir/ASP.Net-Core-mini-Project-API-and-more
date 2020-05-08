0174c72498a1b10dc214c1aa8c6dd44f68b651ab
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-08 22:44:22 +0600

    Service class added

---
+++ TestMVCProject/Models/WelcomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMVCProject.Models
+{
+    public class WelcomeModel
+    {
+        public string name { get; set; }
+        public DateTime datetime { get; set; }
+
+    }
+}
---
+++ TestMVCProject/Services/WelcomeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMVCProject.Services
+{
+    public class WelcomeService
+    {
+    }
+}
