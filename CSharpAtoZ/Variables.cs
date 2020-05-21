d03c90b8a29acff4eb9773eaac1a1498043a8a57
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-22 01:04:21 +0600

    Variables in C#

---
+++ CSharpAtoZ/Variables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sabbir Ahmed";
+            int age;
+            age = 23;
+
+            Console.WriteLine("My name is "+ name + " and I am "+ age +" years old.");
+        }
+    }
+}
