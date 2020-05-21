c59be68de2a8942cc2c1378e2dca545ae398328c
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-22 02:22:15 +0600

    String Manipulation in c#

---
+++ CSharpAtoZ/StringManipulations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class StringManipulations
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sabbir Ahmed";
+            string name1 = "Jahidul Islam";
+            string name2 = "Fahim foysal";
+
+
+            Console.WriteLine("My name is "+ name );
+            Console.WriteLine("My friend first name is " + name1.Substring(0,5));
+            Console.WriteLine("My another friend name start with " + name2[0]);
+        }
+    }
+}
