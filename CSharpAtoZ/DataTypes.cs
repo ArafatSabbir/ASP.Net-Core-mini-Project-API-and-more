8bb8c6692fa9d1a09c46448db561b1796561d5d3
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-22 01:20:24 +0600

    DataTypes in C#

---
+++ CSharpAtoZ/DataTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sabbir Ahmed";
+            int age = 23;
+            char group = 'A';
+            double gpa = 3.00;
+            bool IsMale = true;
+
+            Console.WriteLine("My name is "+ name + " and I am "+ age +" years old." + IsMale + group + gpa);
+        }
+    }
+}
