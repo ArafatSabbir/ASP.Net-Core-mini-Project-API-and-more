2a5240db4eb0d81b949d9059bf5b3cb1cfe20459
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-22 03:10:23 +0600

    Fun With Numbers

---
+++ CSharpAtoZ/FunWithNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class FunWithNumbers
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b= 6 ,c = a+b;
+            double x = 3.30;
+
+
+            Console.WriteLine("Sum of a and b is " + c);
+            Console.WriteLine("Square of 6 is "+Math.Pow(b,2));
+            Console.WriteLine("Round of x is " + Math.Round(x));
+        }
+    }
+}
