02f87f2052f4883cedbd1449e676a6816742a40b
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-22 17:05:32 +0600

    User Input on C#

---
+++ CSharpAtoZ/UserInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class UserInput
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine($"Sum of {a} and {b} is " + Math.Pow(a,b));
+            
+        }
+    }
+}
