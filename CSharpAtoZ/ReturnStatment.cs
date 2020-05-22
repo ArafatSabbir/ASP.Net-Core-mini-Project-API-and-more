596f5950514962da7f56766b00f504aff6cc95f0
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-23 02:35:57 +0600

    Return Statement

---
+++ CSharpAtoZ/ReturnStatment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class ReturnStatment
+    {
+        static void Main(string[] args)
+        {
+            int num = 8;
+            Console.WriteLine(Qube(num));
+
+        }
+
+        static int Qube(int number)
+        {
+            int result = number * number * number;
+            return result;
+        }
+
+
+    }
+}
+
+
+//Sample output
+
+// 512
+
+
