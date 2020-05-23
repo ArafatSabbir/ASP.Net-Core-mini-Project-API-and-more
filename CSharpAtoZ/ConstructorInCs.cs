ddf2e2b0debd007be8a03565e68b62cc1a0845ce
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-24 02:38:35 +0600

    Constructor in C#

---
+++ CSharpAtoZ/ConstructorInCs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class ConstructorInCs
+    {
+        static void Main(string[] args)
+        {
+            var object1 = new Customer("Sabbir Ahmed", 33, true);
+
+            Console.WriteLine($"Customer name is {object1.name}");
+            
+        }
+
+
+    }
+
+    class Customer
+    {
+        public string name;
+        public int id;
+        public bool IsHappy;
+
+        public Customer(string cName, int cId, bool cIsHappy)
+        {
+            name = cName;
+            id = cId;
+            IsHappy = cIsHappy;  
+        }
+
+
+    }
+}
+
+
+//Sample output
+
+//Student name is Sabbir
+
+
+
+
+
+
+
+
