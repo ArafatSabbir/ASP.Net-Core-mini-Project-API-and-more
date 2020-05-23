a3da06e4dfc96097d3d74c3c84517cffe72efc63
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-24 01:25:31 +0600

    2D array in C#

---
+++ CSharpAtoZ/TwoDarray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class TwoDarray
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray =
+            {
+                {1,2,3 },
+                {4,5,6 },
+                {7,8,9 }
+            };
+
+            Console.WriteLine("1st number of the array is : " + myArray[0, 0]);
+            Console.WriteLine("Last number of the array is : " + myArray[2,2]);
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
