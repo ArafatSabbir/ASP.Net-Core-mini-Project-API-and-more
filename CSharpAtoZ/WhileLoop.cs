1a771c954002c5b4943572af414b5f1f957f936d
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-23 21:10:40 +0600

    While Loop in C#

---
+++ CSharpAtoZ/WhileLoop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+
+            while (count < 11)
+            {
+                Console.WriteLine($"{number} X {count} = {number*count}");
+                count++;
+            }
+  
+
+        }
+
+
+    }
+}
+
+//Enter a number : 6
+//6 X 1 = 6
+//6 X 2 = 12
+//6 X 3 = 18
+//6 X 4 = 24
+//6 X 5 = 30
+//6 X 6 = 36
+//6 X 7 = 42
+//6 X 8 = 48
+//6 X 9 = 54
+//6 X 10 = 60
+
+
+
+
+
+
+
