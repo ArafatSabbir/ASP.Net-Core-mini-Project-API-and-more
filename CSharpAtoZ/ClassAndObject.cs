1533228d648005f5611c7b29aa7c3cbb2f1fcad0
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-24 01:58:40 +0600

    Class and Object in C#

---
+++ CSharpAtoZ/ClassAndObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class ClassAndObject
+    {
+        static void Main(string[] args)
+        {
+            var object1 = new Student();
+
+            object1.name = "Sabbir";
+            object1.id = 1;
+            object1.IsHappy = true;
+
+            Console.WriteLine($"Student name is {object1.name}");
+            
+
+        }
+
+
+    }
+
+    class Student
+    {
+        public string name;
+        public int id;
+        public bool IsHappy;
+
+    }
+}
+
+
+//Sample output
+
+//Student name is Sabbir
+
+
+
+
+
+
+
+
