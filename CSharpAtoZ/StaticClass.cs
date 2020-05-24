9b4dbbb3df7e0ebbe5ccdc09556246519053e8cf
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-25 02:19:05 +0600

    Static Class

---
+++ CSharpAtoZ/StaticClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class StaticClass
+    {
+        static void Main(string[] args)
+        {
+            Tools.PrintSomething("Bangladesh");
+        }
+
+
+    }
+
+    static class Tools
+    {
+        public static void PrintSomething( string text)
+        {
+            Console.WriteLine("Use passed text is \"" + text + "\"");
+        }
+
+
+    }
+}
+
+
+//Sample output
+
+//Use passed text is "Bangladesh"
+
+
+
+
+
+
+
+
