14b8e07b5803ccd4ce717a131f22b13fc78b8b97
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-24 21:28:27 +0600

    Getter And Setter in C#

---
+++ CSharpAtoZ/SetterAndGetter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CSharpAtoZ
+{
+    class SetterAndGetter
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Book("Hello World!", 33, 10);
+
+            Console.WriteLine($"Book Ratting is {obj.Ratting}");
+            
+        }
+
+
+    }
+
+    class Book
+    {
+        public string name;
+        public int id;
+        private double ratting;
+
+        public Book(string cName, int cId, double cRatting)
+        {
+            name = cName;
+            id = cId;
+            Ratting = cRatting;  
+        }
+
+        public double Ratting
+        {
+            get { return ratting; }
+
+            set { 
+                if(value <= 5 && value >= 0)
+                {
+                    ratting = value;
+                }
+                else
+                {
+                    ratting = -1;
+                }
+            
+            
+            }
+        }
+
+
+    }
+}
+
+
+//Sample output
+
+//Student name is Sabbir
+
+
+
+
+
+
+
+
