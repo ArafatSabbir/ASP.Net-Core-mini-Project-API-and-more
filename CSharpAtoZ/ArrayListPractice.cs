bffd30d1d06f60ac370a0faa89722752650572eb
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-26 00:33:55 +0600

    Array List Basic.

---
+++ CSharpAtoZ/ArrayListPractice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpAtoZ
+{
+    class ArrayListPractice
+    {
+        static void Main(string[] args)
+        {
+            #region Simple Array List
+
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add("Sabbir");
+            myArrayList.Add("Fahim");
+            myArrayList.Add("Jahid");
+            myArrayList.Add("Saifur");
+
+            myArrayList.Remove("Saifur");
+
+            foreach (object o in myArrayList)
+            {
+                Console.WriteLine(o);
+            }
+
+
+            #endregion
+
+        }
+
+
+    }
+
+}
