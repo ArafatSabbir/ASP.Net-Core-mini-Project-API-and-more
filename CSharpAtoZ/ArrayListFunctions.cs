7b07f7a1b10b9b2b46744c23b6831e6da4d67aea
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-26 20:01:36 +0600

    Array list functions

---
+++ CSharpAtoZ/ArrayListFunctions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpAtoZ
+{
+    class ArrayListFunctions
+    {
+        static void Main(string[] args)
+        {
+            #region Simple Array List
+
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add("Sabbir");
+            myArrayList.Add("Fahim");
+            myArrayList.Add("Jahid");
+            myArrayList.Add("Saifur");
+
+            myArrayList.Remove("Saifur");
+
+            foreach (object o in myArrayList)
+            {
+                Console.WriteLine(o);
+            }
+
+            ArrayList myArrayList2 = new ArrayList();
+
+            myArrayList2.AddRange(myArrayList);
+
+            myArrayList2.AddRange(new object[] {"Bangladesh", "Nepal" , "Bhutan"  });
+
+            myArrayList2.Sort();
+
+            myArrayList2.Reverse();
+
+            foreach (object o in myArrayList2)
+            {
+                Console.WriteLine(o);
+            }
+
+
+
+            #endregion
+
+        }
+
+
+    }
+
+}
