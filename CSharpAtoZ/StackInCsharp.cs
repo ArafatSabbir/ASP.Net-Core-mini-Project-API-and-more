8840a5c9ed6c7a4585f07ce80f02409c9089f43a
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-26 20:41:25 +0600

    Stack in c#

---
+++ CSharpAtoZ/StackInCsharp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpAtoZ
+{
+    class StackInCsharp
+    {
+        static void Main(string[] args)
+        {
+            #region StackInCsharp
+
+            var stack = new Stack();
+
+            stack.Push("Fahim");
+            stack.Push("Sabbir");
+            stack.Push("Jahid");
+            stack.Pop();
+            stack.Pop();
+
+
+
+            foreach (Object o in stack)
+            {
+                
+                Console.WriteLine(o);
+            }
+
+
+
+            #endregion
+
+        }
+
+
+    }
+
+}
