739c2b0266de28dc039b2da6739e300d01ea725d
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-26 20:36:49 +0600

    Queue in C# basic

---
+++ CSharpAtoZ/QueueInCsharp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpAtoZ
+{
+    class QueueInCsharp
+    {
+        static void Main(string[] args)
+        {
+            #region QueueInCsharp
+
+            var q = new Queue();
+
+            q.Enqueue("Sabbir");
+            q.Enqueue("Jahid");
+            q.Enqueue("Fahim");
+            q.Dequeue(); // first item will removed
+
+
+            foreach (Object o in q)
+            {
+                
+                Console.WriteLine(o);
+            }
+
+
+
+            #endregion
+
+        }
+
+
+    }
+
+}
