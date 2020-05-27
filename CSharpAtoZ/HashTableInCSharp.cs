9ba375213d16f9212d6c6043df5e3922a041e3c7
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-27 16:38:59 +0600

    Hash Table in C#

---
+++ CSharpAtoZ/HashTableInCSharp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpAtoZ
+{
+    class HashTableInCSharp
+    {
+        static void Main(string[] args)
+        {
+            #region HashTableInCSharp
+
+            var hashTable = new Hashtable();
+
+            hashTable.Add("Name","Sabbir");
+            hashTable.Add("Id", 33);
+            hashTable.Add("Country", "Bangladesh");
+            hashTable.Add("E-mail", "Sabbir@sabbir.com");
+
+
+
+
+
+            foreach (Object o in hashTable.Keys)
+            {
+                
+                Console.WriteLine(o +"-" +hashTable[o]);
+            }
+
+
+
+            #endregion
+
+        }
+
+
+    }
+
+}
