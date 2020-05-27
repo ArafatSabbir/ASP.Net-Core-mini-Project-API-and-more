091ad22eb250c60efcac4fed1980f50113b21395
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-27 18:09:10 +0600

    Hash Set in C#

---
+++ CSharpAtoZ/HashSetInCSharp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpAtoZ
+{
+    class HashSetInCSharp
+    {
+        static void Main(string[] args)
+        {
+            #region HashSetInCSharp
+
+            var hashSet = new HashSet<string>();
+
+            hashSet.Add("Sabbir");
+            hashSet.Add( "33");
+            hashSet.Add("Bangladesh");
+            hashSet.Add("Sabbir@sabbir.com");
+            hashSet.Add("Bangladesh");
+            hashSet.Add("Bangladesh");
+
+
+
+
+
+            foreach (Object o in hashSet)
+            {
+                
+                Console.WriteLine(o );
+            }
+
+
+
+            #endregion
+
+        }
+
+
+    }
+
+}
