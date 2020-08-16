11dfee83b361638617debfce955dc380d141b5b1
ArafatSabbir <sabbirahmedcse33@gmail.com>
2020-08-16 18:53:48 +0600

    Update dependency.

---
+++ BookList/BookList/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
