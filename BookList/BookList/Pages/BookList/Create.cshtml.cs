d7a1fed070e888f20657512eb4323df5cf41982b
ArafatSabbir <sabbirahmedcse33@gmail.com>
2020-08-18 21:10:32 +0600

    Edit page and its functionality added.

--- BookList/BookList/Pages/BookList/Create.cshtml.cs
+++ BookList/BookList/Pages/BookList/Create.cshtml.cs
@@ -16,11 +16,25 @@ namespace BookList.Pages.BookList
         {
             _db = db;
         }
-
+        [BindProperty]
         public Book Book { get; set; }
         public void OnGet()
         {
 
+        }
+
+        public async Task<ActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
---
+++ BookList/BookList/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookList.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public  Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+            
+        }
+    }
+}
