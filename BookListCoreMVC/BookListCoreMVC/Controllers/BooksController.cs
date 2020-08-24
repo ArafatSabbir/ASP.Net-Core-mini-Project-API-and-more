2c6760b69153caf44368adab9115628b41c10195
ArafatSabbir <sabbirahmedcse33@gmail.com>
2020-08-24 22:25:58 +0600

    Book views .

---
+++ BookListCoreMVC/BookListCoreMVC/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookListCoreMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
