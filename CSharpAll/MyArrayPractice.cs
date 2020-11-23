0b97d4ca8b91321a07616e9675b38c968fd3742c
ArafatSabbir <sabbirahmedcse33@gmail.com>
2020-11-23 13:01:19 +0600

    Array in C#

--- CSharpAll/MyArrayPractice.cs
+++ CSharpAll/MyArrayPractice.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace CSharpAll
 {
     class MyArrayPractice
     {
+        static void Main(string[] args)
+        {
+            
+            ///2D array
+            
+            int[,] arr =
+            {
+                {1,2,3,4},
+                {8,5,6,7},
+                {9,11,22,33}
+            };
+            Console.WriteLine(arr[2,2]);
+
+
+            ///random  array
+
+            int[][] arr2 =
+            {
+                new int[] {1,2,3,4},
+                new int[] {8,5,6,7,22,44,55,77},
+                new int[] {9,11,33}
+            };
+            Console.WriteLine(arr2[2] [2]);
+
+
+        }
+
+        
+        
+
     }
 }
