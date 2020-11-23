8cbd763767079502f20fdb61deefce5e96d93aca
ArafatSabbir <sabbirahmedcse33@gmail.com>
2020-11-23 14:19:32 +0600

    List in C#

---
+++ CSharpAll/ListInCS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAll
+{
+    class ListInCS
+    {
+        static void Main(string[] args)
+        {
+
+            var myLi = new List<int>() { 1,2,3,4};
+            var myLi2 = new List<int>() { 1, 2, 3, 4 };
+
+            if (myLi == myLi2)
+            {
+                Console.WriteLine("Li 1 is Equal li 2. using ==");
+            }
+
+
+            if (myLi.SequenceEqual(myLi2))
+            {
+                Console.WriteLine("List 1 is Equal list 2.using Linq");
+            }
+
+        }
+    }
+}
