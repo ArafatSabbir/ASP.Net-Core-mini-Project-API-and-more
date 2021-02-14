2eda38ea6518df50cb6783426d9d9e7fe075d8a9
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-02-14 17:26:20 +0600

    CS All up

--- CSharpAll/Program.cs
+++ CSharpAll/Program.cs
@@ -6,7 +6,23 @@ namespace CSharpAll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+        }
+        public void ReverseString(char[] s)
+        {
+            int len = s.Length;
+            char[] fn = new char[len];
+            int i = 0;
+            while (i < len / 2)
+            {
+                fn[i] = s[len - 1 - i];
+                fn[len - 1 - i] = s[i];
+                i++;
+            }
+            if (len % 2 == 1) 
+                fn[i] = s[i];
+
+            return String.Join("",fn);
         }
 
 
--- CSharpAll/Solution.cs
+++ CSharpAll/Solution.cs
@@ -1,40 +1,32 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CSharpAll
 {
-    class Solution
+    public class Solution
     {
-        public IList<bool> CheckArithmeticSubarrays(int[] nums, int[] l, int[] r)
+        public int countGoodRectangles(int[][] rectangles)
         {
-            var ans = new List<bool>();
-            for (var i = 0; i < l.Length; i++)
+            int count = 0, maxSide = 0;
+            foreach (int[] r in rectangles)
             {
-                var arr = new int[r[i] - l[i] + 1];
-                Array.Copy(nums, l[i], arr, 0, r[i] - l[i] + 1);
-                Array.Sort(arr);
-                ans.Add(IfArithmetic(arr));
-            }
-
-            return ans;
-        }
-
-        public static bool IfArithmetic(int[] arr)
-        {
-            if (arr.Length <= 1)
-                return false;
-
-            var diff = arr[1] - arr[0];
-            for (var i = 2; i < arr.Length; i++)
-            {
-                if (arr[i] - arr[i - 1] != diff)
+                int side = Math.Min(r[0], r[1]);
+                if (side > maxSide)
                 {
-                    return false;
+                    count = 1;
+                    maxSide = side;
+                }
+                else if (side == maxSide)
+                {
+                    ++count;
                 }
             }
-
-            return true;
+            return count;
         }
+
     }
 }
+
