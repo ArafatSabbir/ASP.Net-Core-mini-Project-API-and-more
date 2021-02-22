11a16136b719d1be65be8802ceded63c579e6696
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-02-22 22:38:17 +0600

    Bubble Sort

--- CSharpAll/TestPlace.cs
+++ CSharpAll/TestPlace.cs
@@ -10,7 +10,12 @@ namespace CSharpAll
         {
             int[] unSortedArr = {2, 10, 1, 6, 3, 5, 7, 12, 8 };
             int[] sortedArr = {1, 2, 3, 5, 6, 7, 8, 10, 12};
-            Console.WriteLine(MyAlgorithms.BinarySearchIterative(unSortedArr,10));
+           
+            int[] res = MyAlgorithms.BubbleSort(arr: unSortedArr);
+            foreach (int a in res)
+            {
+                Console.Write($"{a} ");
+            }
         }
     }
 }
